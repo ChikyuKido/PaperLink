c1fe38ebad19095bd344bc76467de4b1de2eccf2
Philipp Schwarzinger <SCH220239@spengergasse.at>
2025-11-25 10:52:13 +0100

    BACKEND-Adding logging to current system with logrus

--- src/db/db.go
+++ src/db/db.go
@@ -1,29 +1,36 @@
 package db
 
 import (
-	"log"
 	"paperlink/db/entity"
+	"paperlink/util"
 
+	"github.com/sirupsen/logrus"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
+var log = util.GroupLog("DATABASE")
 
 func Init() {
 	db, err := gorm.Open(sqlite.Open("paperlink.db"), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("failed to connect database: %v", err)
+	}
 	_ = db.Exec(`
 		PRAGMA journal_mode=WAL;
 	`)
+
+	err = db.AutoMigrate(
+		&entity.Annotation{}, &entity.AnnotationAction{}, &entity.Category{},
+		&entity.Document{}, &entity.DocumentUser{}, &entity.Notification{},
+		&entity.Tag{}, &entity.User{},
+	)
 	if err != nil {
-		log.Fatal("failed to connect database:", err)
-	}
-	err = db.AutoMigrate(&entity.Annotation{}, &entity.AnnotationAction{}, &entity.Category{}, &entity.Document{},
-		&entity.DocumentUser{}, &entity.Notification{}, &entity.Tag{}, &entity.User{})
-	if err != nil {
-		log.Fatal("failed to migrate database:", err)
+		logrus.WithError(err).Fatal("failed to migrate database")
 		return
 	}
 
+	log.Info("database initialized successfully")
 	DB = db
 }
--- src/main.go
+++ src/main.go
@@ -3,9 +3,16 @@ package main
 import (
 	"paperlink/db"
 	"paperlink/server"
+	"paperlink/util"
+
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
+	// Set custom formatter
+	logrus.SetFormatter(&util.GroupFormatter{})
+	logrus.SetLevel(logrus.InfoLevel)
+
 	db.Init()
 	server.Start()
 }
