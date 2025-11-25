c099f5b227f1e6761a00e9f6996e50e46371fb7b
Kidō <chikyu.kido@posteo.de>
2025-11-25 12:42:06 +0100

    added swagger

--- src/server/routes/auth/router.go
+++ src/server/routes/auth/router.go
@@ -4,7 +4,12 @@ import "github.com/gin-gonic/gin"
 
 func InitAuthRouter(r *gin.Engine) {
 	group := r.Group("/api/v1/auth")
-	group.GET("/test", func(context *gin.Context) {
-		context.JSON(200, gin.H{"test": "test"})
-	})
+	group.GET("/test", Ping)
+}
+
+// @Summary Ping
+// @Success 200 {string} string "pong"
+// @Router /ping [get]
+func Ping(c *gin.Context) {
+	c.String(200, "pong")
 }
